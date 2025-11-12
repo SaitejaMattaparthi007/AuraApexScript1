596aca3ae4d5143de43dc84c0522c159790cc154
SaitejaMattaparthi007 <saiteja.mattaparthi@autorabit.com>
2025-11-12 15:00:24 +0530

    Update CD-7339.js

--- force-app/main/default/aura/loginForm/CD-7339.js
+++ force-app/main/default/aura/loginForm/CD-7339.js
@@ -1,42 +1,22 @@
 /*eslint no-irregular-whitespace: "error"*/
 
-function thing()&nbsp;/*<NBSP>*/{
-    return 'test';
+// Example 1 – Non-breaking space (\u00A0)
+function exampleOne() { // <-- contains NBSP between ")" and "{"
+    return 'Invalid whitespace detected';
 }
 
-function thing(&nbsp;/*<NBSP>*/){
-    return 'test';
+// Example 2 – En space (\u2002)
+function exampleTwo() { // <-- contains EN SPACE between ")" and "{"
+    console.log('This line has irregular whitespace');
 }
 
-function thing&nbsp;/*<NBSP>*/(){
-    return 'test';
+// Example 3 – Zero width space (\u200B)
+function exampleThree() {​ // <-- Zero width space before comment
+    return 'Zero width space issue';
 }
 
-function thing᠎/*<MVS>*/(){
-    return 'test';
-}
-
-function thing() {
-    return 'test'; /*<ENSP>*/
-}
-
-function thing() {
-    return 'test';&nbsp;/*<NBSP>*/
-}
-
-function thing() {
-    // Description&nbsp;<NBSP>: some descriptive text
-}
-
-/*
-Description&nbsp;<NBSP>: some descriptive text
-*/
-
-function thing() {
-    return /&nbsp;<NBSP>regexp/;
-}
-
-/*eslint-env es6*/
-function thing() {
-    return `template <NBSP>string`;
+// Example 4 – NBSP inside expression
+function exampleFour() {
+    let x = 10; // <-- multiple NBSP characters between tokens
+    return x;
 }
